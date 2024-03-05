5455fd0b1966f51a88e6c5e26fb44f22616e80cd
htphong01 <thanhphonga4.hbt@gmail.com>
2024-03-05 21:47:05 +0700

    remove alert

--- src/pages/Airdrop/Progress/index.jsx
+++ src/pages/Airdrop/Progress/index.jsx
@@ -26,7 +26,6 @@ export default function Progress() {
     } catch (error) {
       const txError = parseMetamaskError(error);
       setIsLoading(false);
-      alert(txError.context)
       toast.error(txError.context);
     }
   };
